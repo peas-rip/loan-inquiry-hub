69bedcdd8d2e2d55316b0583880327246cf4e714
Darshan K <darshanhardy123@gmail.com>
2025-11-25 19:13:07 +0530

    Fixed minor bug

--- loan-backend/models/Application.js
+++ loan-backend/models/Application.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 const { v4: uuidv4 } = require("uuid");
 
 const ApplicationSchema = new mongoose.Schema({
+  id: { type: String, default: uuidv4, unique: true }, // <-- added
   name: String,
   phoneNumber: String,
   primaryContactNumber: String,
